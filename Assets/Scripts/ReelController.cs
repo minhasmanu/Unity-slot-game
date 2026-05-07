7692cc5f230a8c14b97a1aba70fb0f9440613815
Minhas P <manuminhas33@gmail.com>
2026-05-07 21:09:43 +0530

    added basic slot reel spinning and infinite symbol looping system

--- Assets/Scripts/ReelController.cs
+++ Assets/Scripts/ReelController.cs
@@ -3,19 +3,41 @@ using UnityEngine;
 
 public class ReelController : MonoBehaviour
 {
-    public float speed = 1000f;
-    public float spinTime = 2f;
+    public float speed = 400f;
+    public float spinTime = 3f;
+
+    public RectTransform[] symbols;
+
+    public float symbolHeight = 50f;
+    public float resetPositionY = 250f;
+    public float bottomLimitY = 70f;
 
     private bool isSpinning = false;
 
     public IEnumerator Spin()
     {
         isSpinning = true;
+
         float timer = 0f;
 
         while (timer < spinTime)
         {
-            transform.Translate(Vector3.down * speed * Time.deltaTime);
+            foreach (RectTransform symbol in symbols)
+            {
+                // Move symbol downward
+                symbol.anchoredPosition += Vector2.down * speed * Time.deltaTime;
+
+                // If symbol goes below limit
+                if (symbol.anchoredPosition.y < bottomLimitY)
+                {
+                    // Move it back to top
+                    symbol.anchoredPosition = new Vector2(
+                        symbol.anchoredPosition.x,
+                        resetPositionY
+                    );
+                }
+            }
+
             timer += Time.deltaTime;
             yield return null;
         }
